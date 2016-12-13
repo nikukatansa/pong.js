fa3f5b10ab4f7b9cfa25aea68e9c40278880734e
nikukatansa <graham@grahamknight.org>
2016-12-13 22:36:28 +0000

    More accurate gameplay

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # pong.js
+
+A p5.js version of the classic game Pong.
+
+## Controls
+
+W/S moves the left paddle up and down.  Up arrow / down arrow moves the right paddle up and down.
+
+## Gameplay
+
+The game starts automatically and continues until one player scores 11 points.
--- sketch.js
+++ sketch.js
@@ -1,9 +1,10 @@
+var MAX_ROUNDS = 11;
 var gameState;
 var padOffset = 50;
 var padLeft, padRight;
 var padHeight = 40;
 var padWidth = 5;
-var padStep = 5;
+var padStep = 10;
 var ballX, ballY;
 var theta; // Y axis (down) is 0, angle measured counterclockwise
 var ballVel;
@@ -12,19 +13,19 @@ var ballR = 9;
 var scoreLeft, scoreRight;
 
 function setup() {
-  createCanvas(640,480);
+  createCanvas(800,600);
   rectMode(RADIUS);
   angleMode(DEGREES);
   textAlign(CENTER);
   scoreLeft = 0;
   scoreRight = 0;
-  gameState = -1;  // Left paddle
+  gameState = random([-1,1]);  // Choose random start direction (-1 to left, 1 to right)
   padLeft = height/2;
   padRight = height/2;
   velScaleFactor = 1.15;
   ballVel = createVector(0,0);
-  ballX = padOffset + (2 * padWidth) + ballR;
-  ballY = height/2;
+  ballX = width/2;
+  ballY = random(2*ballR, height - (2*ballR));
 }
 
 function draw() {
@@ -35,41 +36,23 @@ function draw() {
   // Movement of paddles
   if (keyIsDown(87) && (padLeft - padHeight - padStep) >= 0 ){ // W pressed
     padLeft -= padStep;
-    if (gameState == -1){
-      ballY -= padStep;
-    }
   }
   if (keyIsDown(83) && (padLeft + padHeight + padStep) <= height ) { // S pressed
     padLeft += padStep;
-    if (gameState == -1){
-      ballY += padStep;
-    }
   }
   if (keyIsDown(38) && (padRight - padHeight - padStep) >= 0) { // UP pressed
     padRight -= padStep;
-    if (gameState == 1){
-      ballY -= padStep;
-    }
   }
   if (keyIsDown(40) && (padRight + padHeight + padStep) <= height ) { // DOWN pressed
     padRight += padStep;
-    if (gameState == 1){
-      ballY += padStep;
-    }
   }
-  if (keyIsDown(32)){ // SPACE pressed
-    if (gameState == -1){ // Start round at left paddle
-      theta = 90;
-      power = 1;
-      ballVel = createVector(power * sin(theta), power * cos(theta));
-      gameState = 0;
-    }
-    if (gameState == 1){ // Start round at right paddle
-      theta = 270;
-      power = 1;
-      ballVel = createVector(power * sin(theta), power * cos(theta));
-      gameState = 0;
-    }
+  
+  // Round reset
+  if (gameState != 0){
+    theta = random(120-(90*gameState), 240-(90*gameState)); // [30,150] if gS=1, [210,330] if gS=-1
+    power = 3;
+    ballVel = createVector(power * sin(theta), power * cos(theta));
+    gameState = 0;
   }
   
   // Movement of ball
@@ -83,18 +66,16 @@ function draw() {
     }
     
     if ((ballX + ballR > width-padOffset-(2*padWidth)) && 
-        (abs(ballY-padRight) <= padHeight)) { // RIGHT PADDLE
+        (abs(ballY-padRight) <= padHeight)) { // RIGHT PADDLE bounce
       power *= velScaleFactor;
       theta = map(ballY-padRight, -padHeight, padHeight, 210, 330);
-      console.log("R " + (ballY-padRight) + "=>" + theta);
       ballVel = createVector(power * sin(theta), power * cos(theta));
     }
     
     if ((ballX - ballR < padOffset+(2*padWidth)) &&
-        (abs(ballY-padLeft) <= padHeight)) { // LEFT PADDLE
+        (abs(ballY-padLeft) <= padHeight)) { // LEFT PADDLE bounce
       power *= velScaleFactor;
       theta = map(padLeft-ballY, -padHeight, padHeight, 30, 150);
-      console.log("L " + (ballY-padLeft) + "=>" + theta);
       ballVel = createVector(power * sin(theta), power * cos(theta));
     }
   }
@@ -102,18 +83,24 @@ function draw() {
   // End of round conditions
   if (ballX-ballR <= 0) { 
     scoreRight += 1;
-    ballX = padOffset + (2 * padWidth) + ballR;
-    ballY = padLeft;
+    ballX = width/2;
+    ballY = random(2*ballR, height - (2*ballR));
     ballVel = createVector(0,0);
     gameState = -1;
+    if (scoreRight == MAX_ROUNDS){
+      noLoop(); // End of game, right has won
+    }
   }
   
   if (ballX+ballR >= width) {
     scoreLeft += 1;
-    ballX = width - padOffset - (2 * padWidth) - ballR;
-    ballY = padRight;
+    ballX = width/2;
+    ballY = random(2*ballR, height - (2*ballR));
     ballVel = createVector(0,0);
     gameState = 1;
+    if (scoreLeft == MAX_ROUNDS){
+      noLoop(); // End of game, left has won
+    }
   }
   
   ballX += ballVel.x;
